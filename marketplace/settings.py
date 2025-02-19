a5ad6de13a2477dfd016f6953bab13704ac50eee
Laksh <laksh8396@gmail.com>
2025-02-19 23:09:27 +0530

    fix settings.py, urls.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
