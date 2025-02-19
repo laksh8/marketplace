d45da36c418d996d30e38ffc8af4d22eb90f16f3
Laksh <laksh8396@gmail.com>
2025-02-19 22:41:42 +0530

    remove unneccessary import modify timezone

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 from decouple import config
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
