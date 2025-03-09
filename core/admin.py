034f1c41506c2021043e96c8c556f7994e6211f3
Laksh <laksh8396@gmail.com>
2025-03-10 01:08:58 +0530

    admin sites register

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from core.models import Profile
+from core.models import Category, Order, Product, Profile
 
 # Register your models here.
 
 admin.site.register(Profile)
+# admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Category)
