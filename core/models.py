a897b9fff378dd45390d2f79084373e9a442cb67
Laksh <laksh8396@gmail.com>
2025-04-22 23:54:03 +0530

    Seller base model

--- core/models.py
+++ core/models.py
@@ -7,3 +7,6 @@ class Seller(models.Model):
     # details
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
+
+    def __str__(self):
+            return f"{self.name}"
--- core/views.py
+++ core/views.py
@@ -3,6 +3,6 @@ from .models import Seller
 from django.contrib.auth.models import User
 
 def home(request):
-    seller = Seller.objects.all()
-    return render(request, 'home.html', {'seller': seller})
+    sellers = Seller.objects.all()
+    return render(request, 'home.html', {'sellers': sellers})
 
